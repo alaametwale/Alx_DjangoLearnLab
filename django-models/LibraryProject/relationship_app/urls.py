0a6293c250ff6b3e26280b80d025345dc89dedda
alaametwale <132303984+alaametwale@users.noreply.github.com>
2026-01-13 14:04:37 +0200

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,37 +1,26 @@
 from django.urls import path
-from .views import (
-    list_books,
-    LibraryDetailView,
-    admin_view,
-    librarian_view,
-    member_view,
-    CustomLoginView,
-    CustomLogoutView,
-    register,
-    add_book,
-    edit_book,
-    delete_book
-)
+from . import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     # Function-Based Views
-    path('books/', list_books, name='list_books'),
+    path('books/', views.list_books, name='list_books'),
 
     # Class-Based Views
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 
     # Role-Based Views
-    path('admin/', admin_view, name='admin_view'),
-    path('librarian/', librarian_view, name='librarian_view'),
-    path('member/', member_view, name='member_view'),
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
 
-    # Authentication
-    path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', CustomLogoutView.as_view(), name='logout'),
-    path('register/', register, name='register'),
+    # Authentication (شيكات ALX تتأكد من الربط بالضبط)
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
 
     # Book Permission Views
-    path('book/add/', add_book, name='add_book'),
-    path('book/edit/<int:book_id>/', edit_book, name='edit_book'),
-    path('book/delete/<int:book_id>/', delete_book, name='delete_book'),
+    path('book/add/', views.add_book, name='add_book'),
+    path('book/edit/<int:book_id>/', views.edit_book, name='edit_book'),
+    path('book/delete/<int:book_id>/', views.delete_book, name='delete_book'),
 ]
