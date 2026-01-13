c69b09414ac2fb8703c5c37d030c4e704373d558
alaametwale <132303984+alaametwale@users.noreply.github.com>
2026-01-13 14:08:25 +0200

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -3,23 +3,23 @@ from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    # Function-Based Views
+    # FBV
     path('books/', views.list_books, name='list_books'),
 
-    # Class-Based Views
+    # CBV
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 
-    # Role-Based Views
+    # Role-Based
     path('admin/', views.admin_view, name='admin_view'),
     path('librarian/', views.librarian_view, name='librarian_view'),
     path('member/', views.member_view, name='member_view'),
 
-    # Authentication (شيكات ALX تتأكد من الربط بالضبط)
+    # Auth
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
 
-    # Book Permission Views
+    # Book Permissions
     path('book/add/', views.add_book, name='add_book'),
     path('book/edit/<int:book_id>/', views.edit_book, name='edit_book'),
     path('book/delete/<int:book_id>/', views.delete_book, name='delete_book'),
