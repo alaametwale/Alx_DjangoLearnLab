423b1057ac1b38e1f031b71806054408c517ec5b
alaametwale <132303984+alaametwale@users.noreply.github.com>
2026-01-13 15:05:49 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,15 +1,14 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import DetailView
-from django.contrib.auth.decorators import user_passes_test, permission_required
+from django.contrib.auth.decorators import user_passes_test, permission_required, login_required
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from .models import Library  # ✅ هذا السطر المطلوب حرفيًا
 from .models import Author, Book, Librarian, UserProfile
-from django.views.generic.detail import DetailView
-from .views import list_books
 
 # ---------- Function-Based View ----------
+@login_required
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
@@ -19,6 +18,10 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+    def get_object(self):
+        library_id = self.kwargs.get("pk")
+        return get_object_or_404(Library, pk=library_id)
 
 # ---------- Role-Based Access ----------
 def is_admin(user):
@@ -44,7 +47,7 @@ def member_view(request):
 
 # ---------- User Authentication ----------
 def register(request):
-    form = UserCreationForm(request.POST or None)
+    form = UserCreationForm(request.POST or None)  # ✅ UserCreationForm موجود
     if form.is_valid():
         user = form.save()
         login(request, user)
