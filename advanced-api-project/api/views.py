575e2c11e04817aa4da2d4f47d73cf352cac2608
alaametwale <132303984+alaametwale@users.noreply.github.com>
2026-02-08 00:21:31 +0200

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,45 +1,47 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from .models import Book
-from .serializers import BookSerializer
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework
 from rest_framework.filters import SearchFilter, OrderingFilter
 
+from .models import Book
+from .serializers import BookSerializer
 
-# List all books (Read for all, write for authenticated)
+
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
 
-    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    # Filtering, Searching, Ordering
+    filter_backends = [
+        rest_framework.DjangoFilterBackend,
+        SearchFilter,
+        OrderingFilter
+    ]
+
     filterset_fields = ['title', 'publication_year', 'author']
     search_fields = ['title', 'author__name']
     ordering_fields = ['title', 'publication_year']
 
 
-# Retrieve single book
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
 
 
-# Create book (Authenticated only)
 class BookCreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]
 
 
-# Update book (Authenticated only)
 class BookUpdateView(generics.UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]
 
 
-# Delete book (Authenticated only)
 class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
