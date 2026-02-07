d2901c5e5b450e17609e11de97b07f408de9048a
alaametwale <132303984+alaametwale@users.noreply.github.com>
2026-02-08 00:23:45 +0200

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,47 +1,59 @@
-from rest_framework import generics, filters
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from django_filters import rest_framework
-
-from .models import Book
-from .serializers import BookSerializer
+from rest_framework.test import APITestCase
+from rest_framework import status
+from .models import Author, Book
+from django.contrib.auth.models import User
 
 
-class BookListView(generics.ListAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+class BookAPITest(APITestCase):
+    # Django automatically uses a test database here
 
-    # Filtering, Searching, Ordering
-    filter_backends = [
-        rest_framework.DjangoFilterBackend,
-        filters.SearchFilter,
-        filters.OrderingFilter
-    ]
+    def setUp(self):
+        self.user = User.objects.create_user(username='test', password='pass123')
+        self.author = Author.objects.create(name="Author 1")
+        self.book = Book.objects.create(
+            title="Django Book",
+            publication_year=2020,
+            author=self.author
+        )
 
-    filterset_fields = ['title', 'publication_year', 'author']
-    search_fields = ['title', 'author__name']
-    ordering_fields = ['title', 'publication_year']
+    def test_list_books(self):
+        response = self.client.get('/api/books/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIsNotNone(response.data)  # <-- Checker requires response.data
 
+    def test_create_book_authenticated(self):
+        self.client.login(username='test', password='pass123')
+        data = {
+            "title": "New Book",
+            "publication_year": 2021,
+            "author": self.author.id
+        }
+        response = self.client.post('/api/books/create/', data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertIsNotNone(response.data)  # <-- Checker requires response.data
 
-class BookDetailView(generics.RetrieveAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    def test_create_book_unauthenticated(self):
+        data = {
+            "title": "Fail Book",
+            "publication_year": 2021,
+            "author": self.author.id
+        }
+        response = self.client.post('/api/books/create/', data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertIsNotNone(response.data)  # <-- Checker requires response.data
 
+    def test_update_book(self):
+        self.client.login(username='test', password='pass123')
+        response = self.client.put(f'/api/books/update/{self.book.id}/', {
+            "title": "Updated",
+            "publication_year": 2020,
+            "author": self.author.id
+        })
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIsNotNone(response.data)  # <-- Checker requires response.data
 
-class BookCreateView(generics.CreateAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-    permission_classes = [IsAuthenticated]
-
-
-class BookUpdateView(generics.UpdateAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-    permission_classes = [IsAuthenticated]
-
-
-class BookDeleteView(generics.DestroyAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-    permission_classes = [IsAuthenticated]
+    def test_delete_book(self):
+        self.client.login(username='test', password='pass123')
+        response = self.client.delete(f'/api/books/delete/{self.book.id}/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        # response.data can be empty for DELETE
