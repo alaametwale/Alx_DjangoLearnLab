730a9b7b8b990efba1481f9f7fc578b2f899920b
alaametwale <132303984+alaametwale@users.noreply.github.com>
2026-02-23 21:15:13 +0200

    Update serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
-from django.contrib.auth import authenticate
-from .models import User
+from django.contrib.auth import authenticate, get_user_model
+from rest_framework.authtoken.models import Token
+
+User = get_user_model()
+
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -10,7 +13,11 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ["id", "username", "email", "password", "bio"]
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+        user = get_user_model().objects.create_user(**validated_data)
+
+        # create token for user
+        Token.objects.create(user=user)
+
         return user
 
 
@@ -22,4 +29,7 @@ class LoginSerializer(serializers.Serializer):
         user = authenticate(**data)
         if not user:
             raise serializers.ValidationError("Invalid credentials")
+
+        Token.objects.get_or_create(user=user)
+
         return user
