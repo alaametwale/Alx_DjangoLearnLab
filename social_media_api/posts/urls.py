073ba80eca50c75aa493828c829d796cd04203c5
alaametwale <132303984+alaametwale@users.noreply.github.com>
2026-02-23 23:09:05 +0200

    Update urls.py

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,12 +1,8 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet, FeedView
-
-router = DefaultRouter()
-router.register("posts", PostViewSet)
-router.register("comments", CommentViewSet)
+from django.urls import path
+from .views import FeedView, LikePostView, UnlikePostView
 
 urlpatterns = [
     path("feed/", FeedView.as_view(), name="feed"),
-    path("", include(router.urls)),
+    path("<int:pk>/like/", LikePostView.as_view(), name="like-post"),
+    path("<int:pk>/unlike/", UnlikePostView.as_view(), name="unlike-post"),
 ]
