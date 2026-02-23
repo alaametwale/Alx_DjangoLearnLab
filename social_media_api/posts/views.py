092bf2cbcd7c2585327484b2a981a7813088b0bb
alaametwale <132303984+alaametwale@users.noreply.github.com>
2026-02-23 22:42:13 +0200

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,40 +1,12 @@
-from rest_framework import viewsets, generics, permissions, filters
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.response import Response
-
-from .models import Post, Comment
-from .serializers import PostSerializer, CommentSerializer
+from rest_framework import generics, permissions
+from .models import Post
+from .serializers import PostSerializer
 
 
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all().order_by("-created_at")
-    serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    filter_backends = [filters.SearchFilter, DjangoFilterBackend]
-    search_fields = ["title", "content"]
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
-
-class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all().order_by("-created_at")
-    serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
-
-# ⭐ feed view (required by checker)
 class FeedView(generics.ListAPIView):
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         following_users = self.request.user.following.all()
-
-        return Post.objects.filter(
-            author__in=following_users
-        ).order_by("-created_at")
+        return Post.objects.filter(author__in=following_users).order_by("-created_at")
