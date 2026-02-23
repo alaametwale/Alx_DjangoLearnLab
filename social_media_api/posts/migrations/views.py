6e35ba351e33cdfec5d479a2c3827a03f1980513
alaametwale <132303984+alaametwale@users.noreply.github.com>
2026-02-23 19:51:59 +0200

    Create views.py

---
+++ social_media_api/posts/migrations/views.py
@@ -0,0 +1,28 @@
+from rest_framework import viewsets, filters
+from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+from .permissions import IsOwner
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all().order_by("-created_at")
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated & IsOwner]
+
+    filter_backends = [filters.SearchFilter, DjangoFilterBackend]
+    search_fields = ["title", "content"]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all().order_by("-created_at")
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated & IsOwner]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
